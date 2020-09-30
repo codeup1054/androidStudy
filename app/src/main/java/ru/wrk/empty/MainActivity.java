fccc6a91f6237d6895b0e7ee9ba5bc5e1f1840a9
codeup1054 <codeup1054@gmail.com>
2020-09-30 17:42:52 +0300

    2020-09-30 first Empty Кубик

---
+++ app/src/main/java/ru/wrk/empty/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.wrk.empty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //TODO: 2020-09-30
+    }
+}
